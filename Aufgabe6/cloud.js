42ff59c53d13ee92cbfd8b208f00fc21bdeeeec7
Isa <Isa@Isa-HP.eduroam.hs-furtwangen.de>
2017-12-15 09:44:55 +0100

    code

--- Aufgabe6/cloud.js
+++ Aufgabe6/cloud.js
@@ -1,25 +1,21 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 /* Name:Isabell M�ller
      Matrikel:256034
      Datum:19.11.17
      Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und nicht diktiert. */
-var Aufgabe5;
-(function (Aufgabe5) {
-    var Cloud = (function () {
-        /*new ruft constructor methode auf somit kann beim Konstruieren des Objektes
-                 Informationen uebergeben werden*/
+var Aufgabe6;
+(function (Aufgabe6) {
+    var Cloud = (function (_super) {
+        __extends(Cloud, _super);
         function Cloud(_x, _y) {
-            this.x = _x;
-            this.y = _y;
-            this.color = "#FFFFFF";
+            _super.call(this, _x, _y);
+            this.color = "#99E5D0";
             this.g = Math.random() * 1 + 0.3;
         }
-        Cloud.prototype.sayHello = function () {
-            console.log("Hello Clouds");
-        };
-        Cloud.prototype.update = function () {
-            this.move();
-            this.draw();
-        };
         Cloud.prototype.move = function () {
             if (this.x > 800) {
                 this.x = 0;
@@ -27,21 +23,21 @@ var Aufgabe5;
             this.x += this.g;
         };
         Cloud.prototype.draw = function () {
-            Aufgabe5.crc2.beginPath();
-            Aufgabe5.crc2.arc(this.x, this.y, 30, 0, 2 * Math.PI);
-            Aufgabe5.crc2.fillStyle = this.color;
-            Aufgabe5.crc2.fill();
-            Aufgabe5.crc2.beginPath();
-            Aufgabe5.crc2.arc(this.x + 45, this.y, 30, 0, 2 * Math.PI);
-            Aufgabe5.crc2.fillStyle = this.color;
-            Aufgabe5.crc2.fill();
-            Aufgabe5.crc2.beginPath();
-            Aufgabe5.crc2.arc(this.x + 23, this.y - 20, 25, 0, 2 * Math.PI);
-            Aufgabe5.crc2.fillStyle = this.color;
-            Aufgabe5.crc2.fill();
+            Aufgabe6.crc2.beginPath();
+            Aufgabe6.crc2.arc(this.x, this.y, 30, 0, 2 * Math.PI);
+            Aufgabe6.crc2.fillStyle = this.color;
+            Aufgabe6.crc2.fill();
+            Aufgabe6.crc2.beginPath();
+            Aufgabe6.crc2.arc(this.x + 45, this.y, 30, 0, 2 * Math.PI);
+            Aufgabe6.crc2.fillStyle = this.color;
+            Aufgabe6.crc2.fill();
+            Aufgabe6.crc2.beginPath();
+            Aufgabe6.crc2.arc(this.x + 23, this.y - 20, 25, 0, 2 * Math.PI);
+            Aufgabe6.crc2.fillStyle = this.color;
+            Aufgabe6.crc2.fill();
         };
         return Cloud;
-    }());
-    Aufgabe5.Cloud = Cloud;
-})(Aufgabe5 || (Aufgabe5 = {}));
+    }(Aufgabe6.MovingObject));
+    Aufgabe6.Cloud = Cloud;
+})(Aufgabe6 || (Aufgabe6 = {}));
 //# sourceMappingURL=cloud.js.map
